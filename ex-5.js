720e26d56b1dc09ed1bdca9ff43fad9c963eb070
paramee <huhaliza@gmail.com>
2024-06-01 22:20:07 +0700

    Finshed

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let itemPrice = 27000;
+let itemDiscountPrice = (itemPrice * 25) / 100;
+let itemPriceAfterDiscount = itemPrice - itemDiscountPrice;
+
+console.log(itemPriceAfterDiscount);
